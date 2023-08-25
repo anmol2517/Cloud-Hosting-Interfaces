29e013677a494595aaf6de5d4df99f21a94b9f65
Anmol Kumar <110680449+anmol2517@users.noreply.github.com>
2023-08-25 18:06:47 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const collapsibles = document.querySelectorAll(".collapsible");
+collapsibles.forEach((item) =>
+  item.addEventListener("click", function () {
+    this.classList.toggle("collapsible--expanded");
+  })
+);
+
+
+
+
